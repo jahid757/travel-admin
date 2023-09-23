0068892e719db56355035a8d2a38fec498635484
jahid757 <74687787+jahid757@users.noreply.github.com>
2023-09-23 23:34:16 +0600

    update

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+"use strict"
+
+function renderItineraryForm(e){
+    const numberOfItineraryItems = document.getElementById('optionId').value;
+    const parent = document.getElementById('itinerary_form')
+    let html;
+    for (let i = 0; i < numberOfItineraryItems; i++) {
+        html+=`
+        <h5>Itinerary <span>${i+1}</span></h5>
+        <div class="form-group w-100">
+          <label for="tour-title">Title</label>
+          <input
+            id="deals-title"
+            type="text"
+            class="form-control input-default"
+            placeholder="Title "
+          />
+        </div>
+        <div class="form-group">
+          <label for="tour-desc">Description</label>
+          <textarea
+            class="form-control h-150px"
+            rows="6"
+            id="deals-desc"
+            placeholder="Description"
+          ></textarea>
+        </div>
+        `
+    }
+
+    parent.innerHTML = '';
+    parent.innerHTML = html;
+    // console.log(html)
+
+}
